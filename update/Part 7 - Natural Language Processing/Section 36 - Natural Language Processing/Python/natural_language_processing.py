ddd4735ce7ba49841210625c77688d927cedf111
kevinespinozav <kevin.espinoza@usach.cl>
2024-04-22 19:36:40 -0400

    Tarea NLP

--- update/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Python/natural_language_processing.py
+++ update/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Python/natural_language_processing.py
@@ -25,7 +25,6 @@ for i in range(0, 1000):
   review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
   review = ' '.join(review)
   corpus.append(review)
-print(corpus)
 
 # Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
@@ -33,10 +32,14 @@ cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, -1].values
 
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
+
+#------------- Naive Bayes -----------------
+'''
 # Training the Naive Bayes model on the Training set
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
@@ -44,10 +47,83 @@ classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
-accuracy_score(y_test, y_pred)
+print(accuracy_score(y_test, y_pred))
+print(precision_score(y_test, y_pred))
+print(recall_score(y_test, y_pred))
+print(f1_score(y_test, y_pred))
+'''
+
+# ------------ Kernel SVM --------------------
+# Training the Kernel SVM model on the Training set
+
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print(precision_score(y_test, y_pred))
+print(recall_score(y_test, y_pred))
+print(f1_score(y_test, y_pred))
+
+#------------- Random Forest -----------------
+
+# Training the Random Forest Classification model on the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print(precision_score(y_test, y_pred))
+print(recall_score(y_test, y_pred))
+print(f1_score(y_test, y_pred))
+
+
+#------------ Decision tree -----------------
+
+# Training the Decision Tree Classification model on the Training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+print(precision_score(y_test, y_pred))
+print(recall_score(y_test, y_pred))
+print(f1_score(y_test, y_pred))
+
+
+# -------------- CART --------------------
+'''
+from sklearn.tree import DecisionTreeClassifier
+cart_model = DecisionTreeClassifier(random_state=0).fit(X_train, y_train)
+#y_pred for Confusion Matrix  :
+y_pred = cart_model.predict(X_test)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+'''
